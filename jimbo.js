ac78ff54f8fc4ca09e34d68b0d9afb9ec7eeb28b
Ali Farhadi <a.farhadi@gmail.com>
2011-08-27 14:26:16 +0430

    Updating server script.

--- jimbo.js
+++ jimbo.js
@@ -19,25 +19,53 @@ app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
 
 io = io.listen(app);
 
+var objects = [];
 var players = [];
+var SPEED = 4;
+
+var removePlayer = function(player) {
+	players.splice(players.indexOf(player), 1);
+	socket.broadcast.emit('leave', player.id);
+}
 
 io.sockets.on('connection', function(socket) {
-	var	player = {id: Date.now() + Math.random(), x:405 , y:100, vx:0, vy:0};
 	socket.on('join', function(nickname) {
+		var	player = {id: Date.now() + Math.random(), x:405 , y:100, blood: 100};
 		player.nickname = nickname;
 		players.push(player);
 		socket.emit('players', players, player.id);
 		socket.broadcast.emit('player', player);
+		socket.on('up', function() {
+			player.y += SPEED;
+		});
+		socket.on('down', function() {
+			player.y -= SPEED;
+		});
+		socket.on('right', function() {
+			player.x += SPEED;
+			if (player.x < 0) {
+				removePlayer(player);
+			}
+		});
+		socket.on('left', function() {
+			player.x -= SPEED;
+			if (player.x > 809) {
+				removePlayer(player);
+			}
+		});
+		socket.on('fire', function() {
+			
+		});
 		socket.on('disconnect', function() {
-			players.splice(players.indexOf(player), 1);
-			socket.broadcast.emit('leave', player.id);
+			removePlayer(player);
 		});
 	});
 });
 
 var frameInterval = setInterval(function() {
 	players.forEach(function(player) {
-		player.x += player.vx;
-		player.y += player.vy;
+		player.y += SPEED;
 	});
-}, 40);
+
+	io.sockets.emit('frame', players, objects);
+}, 34);
