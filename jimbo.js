47a89cfb83e139fd5c43376d6c8bfb9633993764
Ali Farhadi <a.farhadi@gmail.com>
2011-08-27 06:22:58 +0430

    Use port 80 in production 8000 in development.

--- jimbo.js
+++ jimbo.js
@@ -1,8 +1,21 @@
-var net = require('net');
+require('nko')('oLjV929INRhZXw/I');
+var express = require('express');
+var io = require('socket.io');
 
-var server = net.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+var app = express.createServer();
+app.use(app.router);
+app.use(express.static(__dirname + '/'));
+
+app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+	console.log('Ready');
+
+	// if run as root, downgrade to the owner of this file
+	if (process.getuid() === 0)
+		require('fs').stat(__filename, function(err, stats) {
+			if (err) return console.log(err)
+			process.setuid(stats.uid);
+		});
 });
 
-server.listen(8080);
+io = io.listen(app);
+
